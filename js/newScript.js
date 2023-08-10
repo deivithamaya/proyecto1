498bd09cb53730c946edf1fd8a3ec7e809f6d427
buho <amayadeivith@gmail.com>
2023-08-10 18:05:20 -0500

    cambio, falta por mejorar front-end

--- js/newScript.js
+++ js/newScript.js
@@ -1,17 +1,17 @@
 import { Messange as classMessage } from "./message.js";
 
-console.log("entre");   
-document.getElementById('button').onclick=newRandomMessage;
+let button=document.getElementById("button");
 let param = document.getElementById("param");
-param.style.display="none";
+
+
+button.onclick=newRandomMessage;
+
+
 
 function  newRandomMessage(){   
     let newMessage = new classMessage(null,null,null,"happy message");
-    
     param.innerHTML=newMessage.newMessage();
     param.style.display="block";
-    
-
 }
 
 
